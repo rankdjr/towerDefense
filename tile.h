e7ac3dc40663f4c15b4d01273f6f60cec0286e24
Doug Rank <drank@csub.edu>
2022-03-30 16:57:55 -0700

    fixed TileGrid constructor to draw array layout

--- tile.h
+++ tile.h
@@ -44,15 +44,17 @@ public:
 		width = 10;
 		//
 		//allocate 2d array for map tiles
-		for (int i = 0; i < height; i++) {
-			for (int j = 0; j < width; j++) {
-				if (newMap[j][i] == 0)
+		for (int i = 0; i < width; i++) {
+			for (int j = 0; j < height; j++) {
+				int row = (width - 1) - j; //start reading vals from top of 2d array
+				if (newMap[row][i] == 0)
 					map[i][j] = Tile(&grass, i*64, j*64, 64, 64);
 				else
 					map[i][j] = Tile(&dirt, i*64, j*64, 64, 64);
 			}
 		}
 	}
+
 	void draw() {
 		int width = 10;
 		int height = 10;
@@ -63,6 +65,7 @@ public:
 			}
 		}
 	}
+
     void setTile(Tile t, int x, int y, int height, int width) {
             drawQuadTex(*t.texture, x*64, y*64, t.width, t.height);
     }
