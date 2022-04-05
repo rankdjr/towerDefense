ff3ef0dc38ddba76117d80e81bfee81db7e35f69
Doug Rank <drank@csub.edu>
2022-04-05 01:45:56 -0700

    added showTowerRange to render

--- main.cpp
+++ main.cpp
@@ -12,6 +12,7 @@
 #include <unistd.h>
 #include <cstring>
 #include <GL/glx.h>
+#include <math.h>
 #include "fonts.h"
 #include "global.h"
 #include "image.h"
@@ -175,6 +176,12 @@ void render()
 {
 	grid.draw();
 	enemy->Draw();
+	if (g.showTowerRange) {
+		int mapi = g.xMousePos/64;
+		int mapj = 9-g.yMousePos/64;
+		Tile *t = grid.getTile(mapi,mapj);
+		t->tower.showRange();
+	}
 	if (g.gameState == BUILD) {
 		//get tile based off of mouse position
 		grid.drawTileOutline();
