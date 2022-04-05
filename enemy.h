6210f821182082663868ba0883118796034857e5
Kenneth Wood <kwood@odin.cs.csubak.edu>
2022-04-04 22:41:21 -0700

    Multiple enemies

--- enemy.h
+++ enemy.h
@@ -7,16 +7,16 @@ class Enemy {
 public:
     int x, y, width, height, health;
     float speed;
-    double delay;
     int dir;
-
+    bool alive;
+    Enemy() : x(0), y(0), width(0), height(0), speed(0), dir(0), health(0), alive(0);
+    {}
     Enemy(int x, int y, int width, int height, float speed, int dir){
         this->x = x*64;
         this->y = y*64;
         this->width = width;
         this->height = height;
         this->speed = speed;
-        this->delay =.15;
         // 0= right, 1= down, 2= left, 3=up
         this->dir = dir;
     }
