42ac698486cb3de13d8c86118e53afc4b0c36532
Doug Rank <drank@csub.edu>
2022-04-07 13:29:51 -0700

    rm warnings, init health + alive in constructor

--- enemy.h
+++ enemy.h
@@ -5,13 +5,14 @@
 
 class Enemy {
 public:
-    int x, y, width, height, health;
+    float x, y; 
+    int width, height, health;
     float speed;
     int dir;
     bool alive;
-    Enemy() : x(0), y(0), width(0), height(0), speed(0), dir(0), health(0), alive(0)
+    Enemy() : x(0), y(0), width(0), height(0), health(0), speed(0), dir(0), alive(0)
     {}
-    Enemy(int x, int y, int width, int height, float speed, int dir){
+    Enemy(float x, float y, int width, int height, float speed, int dir){
         this->x = x*64;
         this->y = y*64;
         this->width = width;
@@ -19,11 +20,12 @@ public:
         this->speed = speed;
         // 0= right, 1= down, 2= left, 3=up
         this->dir = dir;
+        health = 100;
+        alive = 1;
     }
 
     void Draw() {
-    
-       drawQuad(x,y,width,height);
+        drawQuad(x,y,width,height);
     } 
 };
 
