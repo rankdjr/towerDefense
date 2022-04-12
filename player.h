12c0b7a18aebc5bbc37a71233c429f7681f2b0c4
Doug Rank <drank@csub.edu>
2022-04-11 23:09:25 -0700

    add player class

---
+++ player.h
@@ -0,0 +1,58 @@
+#ifndef _PLAYER_H_
+#define _PLAYER_H_
+
+#include <vector>
+#include "global.h"
+#include "tower.h"
+#include "tile.h"
+
+class Player {
+public:
+    int hp;
+    int funds;
+    std::vector<Tower> towers;
+
+    Player();
+    void addFunds(int amt);
+    void addTower(Tile tile);
+    void removeTower(Tile tile);
+    void updateHP(int dmg);
+} player;
+
+Player::Player()
+{
+    hp = 10;
+    funds = 100;
+}
+
+void Player::addFunds(int amt)
+{
+    funds += amt;
+}
+
+void Player::addTower(Tile tile)
+{
+    towers.push_back(tile.tower);
+}
+
+void Player::removeTower(Tile tile)
+{
+    if (towers.empty())
+        return;
+    //
+    //loop through towers and find matching x,y to tile paramater
+    //once match is found, remove from vector and add funds to player
+    for (int i = 0; i < towers.size(); i++) {
+        if (towers[i].x == tile.x && towers[i].y == tile.y)
+            towers.erase(towers.begin()+i);
+    }
+    int sellCost = 0;
+    player.addFunds(sellCost);
+}
+
+void Player::updateHP(int dmg)
+{
+    hp -= dmg;
+}
+
+#endif //_PLAYER_H_
